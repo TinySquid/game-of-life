3f8f887df21fe62ca1bdbea1a6828759c512c456
Michael Nunes <tinysquid.git@gmail.com>
2020-05-27 10:41:13 -0700

    First-pass for Game class. Can now draw grid on canvas

--- js/game.js
+++ js/game.js
@@ -1,1 +1,56 @@
-console.log("Hello World");
+/**
+ * The Game class handles all the logic
+ * and drawing of the grid & cells.
+ */
+
+import { STATE, StateMachine } from "./GameState.js";
+import Grid from "./Grid.js";
+
+export default class Game {
+  constructor(gridWidth = 100, gridHeight = 100, cellWidth = 8, cellHeight = 8, targetDelay = 10) {
+    this.gameState = new StateMachine();
+    this.generation = 0;
+    this.grid = new Grid(gridWidth, gridHeight, cellWidth, cellHeight);
+
+    const canvas = document.getElementById("game");
+
+    this.context = canvas.getContext("2d");
+
+    setInterval(this.runGameLoop.bind(this), targetDelay);
+  }
+
+  pause() {
+    this.gameState.transitionTo(STATE.PAUSED);
+  }
+
+  play() {
+    this.gameState.transitionTo(STATE.PLAYING);
+  }
+
+  stop() {
+    this.gameState.transitionTo(STATE.STOPPED);
+  }
+
+  runGameLoop() {
+    // TODO Game loop logic
+    // game loop functionality is dynamic to the game's current state
+    switch (this.gameState.state) {
+      case STATE.PLAYING: {
+        // TODO Run simulation step
+        this.grid.draw(this.context);
+        break;
+      }
+      case STATE.PAUSED: {
+        // TODO Allow for toggling cells via mouse
+        break;
+      }
+      case STATE.STOPPED: {
+        // TODO Clear grid, reset game
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+}
