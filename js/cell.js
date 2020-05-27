88cbd153b5fceda07f229db6d4c12da04b7470db
Michael Nunes <tinysquid.git@gmail.com>
2020-05-27 10:36:00 -0700

    First-pass Cell implementation

--- js/cell.js
+++ js/cell.js
@@ -2,27 +2,24 @@
  * A cell is an individual "pixel" with basic attributes
  * like width, height, colors for living state & deadstate.
  * In the future I'd like to add the ability to track how
- * many generations a cell lived before dieing.
+ * many generations a cell lived before dying.
  */
 export default class Cell {
-  constructor(width, height, liveColor, deadColor, isAlive) {
+  constructor(width = 8, height = 8, liveColor = "white", deadColor = "rgb(18, 18, 18)", isAlive = false) {
     this.width = width;
     this.height = height;
     this.liveColor = liveColor;
     this.deadColor = deadColor;
-    this.isAlive = isAlive;
-  }
-
-  // Getter for living state
-  get isAlive() {
-    return this.isAlive;
+    this.isAlive = true;
   }
 
   // Draw cell onto canvas
-  draw() {
-    // TODO implement cell draw method
-    // always draw cell, but draw with color
-    // matching living state
+  draw(context, x, y) {
+    // Determine color by state
+    this.isAlive ? (context.fillStyle = this.liveColor) : (context.fillStyle = this.deadColor);
+    // Draw to context
+    context.fillRect(x, y, x + this.width, y + this.height);
+    context.strokeRect(x, y, x + this.width, y + this.height);
   }
 
   // Kill the cell >:)
