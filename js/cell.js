ca0ee890809b61188e58fdd8a07c82eca11f453f
Michael Nunes <tinysquid.git@gmail.com>
2020-05-26 19:55:28 -0700

    Further implement the Cell class

--- js/cell.js
+++ js/cell.js
@@ -4,11 +4,34 @@
  * In the future I'd like to add the ability to track how
  * many generations a cell lived before dieing.
  */
-class Cell {
-  constructor(width, height, liveColor, deadColor) {
+export default class Cell {
+  constructor(width, height, liveColor, deadColor, isAlive) {
     this.width = width;
     this.height = height;
     this.liveColor = liveColor;
     this.deadColor = deadColor;
+    this.isAlive = isAlive;
+  }
+
+  // Getter for living state
+  get isAlive() {
+    return this.isAlive;
+  }
+
+  // Draw cell onto canvas
+  draw() {
+    // TODO implement cell draw method
+    // always draw cell, but draw with color
+    // matching living state
+  }
+
+  // Kill the cell >:)
+  kill() {
+    this.isAlive = false;
+  }
+
+  // CONJURATION 100
+  resurrect() {
+    this.isAlive = true;
   }
 }
