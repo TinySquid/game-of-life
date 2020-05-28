db6178c1521fe36b2b0d49cff5e830ddddc23239
Michael Nunes <tinysquid.git@gmail.com>
2020-05-28 13:16:11 -0700

    Hook up buttons to control the game state

--- js/index.js
+++ js/index.js
@@ -5,9 +5,21 @@ import preset from "./presets.js";
 
 const canvas = document.getElementById("canvas");
 
+// Create game instance
 // targetDelay is how fast the iterations will be rendered
 const gameOfLife = new Game(canvas, 250, preset.blank);
 
-// Start!
-gameOfLife.play();
-gameOfLife.pause();
+// Control buttons
+const playBtn = document.getElementById("play")
+const pauseBtn = document.getElementById("pause")
+const stopBtn = document.getElementById("stop")
+
+playBtn.addEventListener("click", (e) => {
+  gameOfLife.play();
+})
+pauseBtn.addEventListener("click", (e) => {
+  gameOfLife.pause();
+})
+stopBtn.addEventListener("click", (e) => {
+  gameOfLife.stop();
+})
