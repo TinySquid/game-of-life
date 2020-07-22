55a823c40f4a459d831b58a0138bd69ebf001d09
Michael Nunes <tinysquid.git@gmail.com>
2020-07-22 16:46:27 -0700

    Add some clarifying comments to the StateMachine class

--- public/js/GameState.js
+++ public/js/GameState.js
@@ -13,24 +13,30 @@ export class StateMachine {
   transitionTo(newState) {
     switch (newState) {
       case STATE.PAUSED: {
+        // Can only pause if we are currently in play state
         if (this.state === STATE.PLAYING) {
           this.state = newState;
         }
         break;
       }
       case STATE.PLAYING: {
+        // Can play from basically any state
         if (this.state === STATE.PAUSED || this.state === STATE.STOPPED || this.state === STATE.IDLE) {
           this.state = newState;
         }
         break;
       }
       case STATE.STOPPED: {
+        // Can stop only if we are playing or paused (stop clears the board, whereas pause does not)
         if (this.state === STATE.PLAYING || this.state === STATE.PAUSED) {
           this.state = newState;
         }
         break;
       }
       case STATE.IDLE: {
+        /*
+        The only time the state moves from any other state back to idle is when we are stopped, this way we clear the board and wait for a new state
+        */
         if (this.state === STATE.STOPPED) {
           this.state = newState;
         }
