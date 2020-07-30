0ad7e69ad5103b5cf70a2864c9a5065247b3a54f
Michael Nunes <tinysquid.git@gmail.com>
2020-07-30 16:17:30 -0700

    Refactor game loop to use requestAnimationFrame over setInterval

--- public/js/Game.js
+++ public/js/Game.js
@@ -7,50 +7,34 @@ import { StateMachine, STATE as GAME_STATE } from "./SM/GameState.js";
 import Grid from "./Grid/Grid.js";
 
 export default class Game {
-  /* 
-  targetDelay (ms) -> Delay between game loop iteration
-  preset -> Grid preset from JSON string
-  */
-  constructor(targetDelay, preset = null) {
+  constructor() {
+    // Barebones setup needs a - grid, generation counter, statemachine
     this.generation = 0;
-    this.grid = new Grid(preset);
+    this.grid = new Grid();
     this.gameState = new StateMachine();
+    this.gameLoopIntervalId = null;
 
     // Setup mouse click detection on canvas so we can toggle individual cells
-    canvas.addEventListener("mousedown", (e) => {
-      const rect = canvas.getBoundingClientRect();
+    this.enableCanvasClickEvent();
+  }
 
-      // Normalize mouse coords relative to canvas
-      const x = Math.floor(e.clientX - rect.left);
-      const y = Math.floor(e.clientY - rect.top);
+  start(targetDelay) {
+    // When using setInterval we need to use .bind to maintain reference to 'this' (game class instance)
+    // setInterval(this.runGameLoop.bind(this), targetDelay);
+    requestAnimationFrame(this.runGameLoop.bind(this, targetDelay));
+  }
 
-      const cell = this.grid.getCellAtCoord(x, y);
-
-      // Can only edit the grid when game is paused / stopped / idle
-      if (this.gameState.state != GAME_STATE.PLAYING) {
-        if (cell) {
-          // Toggle cell state
-          if (cell.isAlive) {
-            cell.kill();
-          } else {
-            cell.resurrect(); // Turn on the cell
-          }
-        }
-      }
-    });
-
-    // Run the game loop at interval set by targetDelay (ms)
-    // Using .bind to maintain reference to 'this' (game class instance)
-    setInterval(this.runGameLoop.bind(this), targetDelay);
+  usePreset(preset) {
+    this.grid.generateUsingPreset(preset);
   }
 
   /* GAME CONTROLS */
-  pause() {
-    this.gameState.transitionTo(GAME_STATE.PAUSED);
-  }
-
   play() {
     this.gameState.transitionTo(GAME_STATE.PLAYING);
+  }
+
+  pause() {
+    this.gameState.transitionTo(GAME_STATE.PAUSED);
   }
 
   reset() {
@@ -67,9 +51,11 @@ export default class Game {
   }
 
   /* GAME LOGIC ITERATION HANDLER */
-  runGameLoop() {
-    // Game loop functionality is dynamic to the game's current state
+  runGameLoop(targetDelay) {
+    // Get start time
+    const t0 = performance.now();
 
+    // Game loop functionality is dynamic to the game's current state
     switch (this.gameState.state) {
       case GAME_STATE.PLAYING: {
         //* Run simulation step
@@ -150,5 +136,48 @@ export default class Game {
 
     // Draw grid
     this.grid.draw();
+
+    //* All code below is just to keep us within our targetDelay
+
+    if (performance.now() - t0 < targetDelay * 100) {
+      // Start a wait interval loop because we haven't reached targetDelay yet
+      this.gameLoopIntervalId = setInterval(this.waitLoop.bind(this, t0, targetDelay), 10);
+    } else {
+      requestAnimationFrame(this.runGameLoop.bind(this, targetDelay));
+    }
+  }
+
+  //* Keeps us at or above targetDelay for game loop iteration and rendering
+  waitLoop(startTime, targetDelay) {
+    if (performance.now() - startTime >= targetDelay) {
+      // Turn off wait loop and go back to iterate game loop again
+      clearInterval(this.gameLoopIntervalId);
+      requestAnimationFrame(this.runGameLoop.bind(this, targetDelay));
+    }
+  }
+
+  enableCanvasClickEvent() {
+    canvas.addEventListener("mousedown", (e) => {
+      const rect = canvas.getBoundingClientRect();
+
+      // Normalize mouse coords relative to canvas
+      const x = Math.floor(e.clientX - rect.left);
+      const y = Math.floor(e.clientY - rect.top);
+
+      const cell = this.grid.getCellAtCoord(x, y);
+
+      // Can only edit the grid when game is not in play
+      if (this.gameState.state != GAME_STATE.PLAYING) {
+        // Sanity check, can probably remove...
+        if (cell) {
+          // Toggle cell state
+          if (cell.isAlive) {
+            cell.kill();
+          } else {
+            cell.resurrect();
+          }
+        }
+      }
+    });
   }
 }
