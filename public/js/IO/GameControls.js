afe74c9d20c8e051a6fe131132ad7dfafbfb37b5
Michael Nunes <tinysquid.git@gmail.com>
2020-07-30 14:11:07 -0700

    Create module that exports all the game input controls

---
+++ public/js/IO/GameControls.js
@@ -0,0 +1,41 @@
+// Control Buttons
+const playBtn = document.getElementById("play-btn");
+const pauseBtn = document.getElementById("pause-btn");
+const resetBtn = document.getElementById("reset-btn");
+const clearBtn = document.getElementById("clear-btn");
+
+// Save / Load Grid Buttons
+const saveBtn = document.getElementById("save-btn");
+const loadBtn = document.getElementById("load-btn");
+
+// Game Simulation Speed Slider
+const speedInput = document.getElementById("game-speed-input");
+
+// Grid & Cell Controls
+const gridSizeInput = document.getElementById("grid-size-input");
+const cellSizeInput = document.getElementById("cell-size-input");
+
+// Cell Color Controls
+const randomColorCheckBox = document.getElementById("random-colors-check");
+const customColorLiving = document.getElementById("custom-color-living-input");
+const customColorDead = document.getElementById("custom-color-dead-input");
+
+module.exports = {
+  // Gameplay buttons
+  playBtn,
+  pauseBtn,
+  resetBtn,
+  clearBtn,
+  // Save / Load
+  saveBtn,
+  loadBtn,
+  // Sim Speed
+  speedInput,
+  // Grid / Cell Sizing
+  gridSizeInput,
+  cellSizeInput,
+  // Cell Color Controls
+  randomColorCheckBox,
+  customColorLiving,
+  customColorDead,
+};
