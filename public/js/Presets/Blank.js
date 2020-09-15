f9c8c19c920638d8796934a0fe81341ae414174a
Michael Nunes <tinysquid.git@gmail.com>
2020-09-14 20:18:10 -0700

    Refactors the blank preset

--- public/js/Presets/Blank.js
+++ public/js/Presets/Blank.js
@@ -1,3 +1,7 @@
+function emptyGrid(width, height) {
+  return new Array(width).fill(new Array(height).fill(0));
+}
+
 const blankPreset = {
   gridWidth: 50,
   gridHeight: 50,
@@ -5,19 +9,9 @@ const blankPreset = {
   cellHeight: 16,
   liveColor: "white",
   deadColor: "black",
-  cells: (() => {
-    // Really wish we could preinitialize arrays in JS...
-    const columns = [];
-    for (let y = 0; y < 100; y++) {
-      const row = [];
-      for (let x = 0; x < 100; x++) {
-        row.push(0);
-      }
-      columns.push(row);
-    }
-
-    return columns;
-  })(),
 };
+
+// Add after to get access to obj props
+blankPreset.cells = emptyGrid(blankPreset.gridWidth, blankPreset.gridHeight);
 
 export default blankPreset;
